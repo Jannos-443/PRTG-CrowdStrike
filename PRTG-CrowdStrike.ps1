65f01d97d9274d7d0611810dd2945cea04d7fe86
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2022-02-07 12:20:24 +0100

    First

---
+++ PRTG-CrowdStrike.ps1
@@ -0,0 +1,169 @@
+param(
+    [string]$Hostname = "https://api.eu-1.crowdstrike.com",
+    [string]$ClientId = '',
+    [string]$ClientSecret = '',
+    [string]$IgnorePattern = ''
+)
+
+#Catch all unhandled Errors
+trap{
+    $Output = "line:$($_.InvocationInfo.ScriptLineNumber.ToString()) char:$($_.InvocationInfo.OffsetInLine.ToString()) --- message: $($_.Exception.Message.ToString()) --- line: $($_.InvocationInfo.Line.ToString()) "
+    $Output = $Output.Replace("<","")
+    $Output = $Output.Replace(">","")
+    Write-Output "<prtg>"
+    Write-Output "<error>1</error>"
+    Write-Output "<text>$Output</text>"
+    Write-Output "</prtg>"
+    Exit
+}
+
+# Error if there's anything going on
+$ErrorActionPreference = "Stop"
+
+# Import Crowdstrike Powershell module
+try {
+    Import-Module -Name PSFalcon -ErrorAction Stop
+} catch {
+    Write-Output "<prtg>"
+    Write-Output " <error>1</error>"
+    Write-Output " <text>Error Loading PSFalcon Powershell Module ($($_.Exception.Message))</text>"
+    Write-Output "</prtg>"
+    Exit
+}
+
+if($ClientId -eq "")
+    {
+    Write-Error -Message "-ClientId is empty or not specified"
+    }
+
+if($ClientSecret -eq "")
+    {
+    Write-Error -Message "-ClientSecret is empty or not specified"
+    }
+
+if($Hostname -eq "")
+    {
+    Write-Error -Message "-Hostname is empty or not specified"
+    }
+
+$xmlOutput = '<prtg>'
+
+# Authenticate with Crowdstrike API
+Request-FalconToken -ClientId $ClientId -ClientSecret $ClientSecret -Hostname $Hostname
+
+
+#Test Falcon Token
+if(-not ((Test-FalconToken).Token))
+    {
+    Write-Error -Message "Token not Valid"
+    }
+
+#Start Region CrowdScore
+    #CrowdScore Latest
+
+    $Scores = Get-FalconScore -Sort timestamp.desc -Limit 6 
+    $CrowdScore = $Scores | Select-Object -First 1 -ExpandProperty Score
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>CrowdScore</channel>
+        <value>$($CrowdScore)</value>
+        <unit>Count</unit>
+        </result>"	
+
+
+    #Crowdstore adjusted last hour
+    $Crowdscore_Changed = ($Scores | Measure-Object -Property adjusted_score -Sum).Sum
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>CrowdScore changed last hour</channel>
+        <value>$($Crowdscore_Changed)</value>
+        <unit>Count</unit>
+        </result>"	
+#End Region CrowdScore
+
+
+#Start Region Detections
+    #The name used in the UI to determine the severity of the detection. Values include Critical, High, Medium, and Low
+    $DetectionsLow = Get-FalconDetection -Filter "status:'new' + max_severity_displayname: 'Low'" -Total
+    $DetectionsMedium = Get-FalconDetection -Filter "status:'new' + max_severity_displayname: 'Medium'" -Total
+    $DetectionsHigh = Get-FalconDetection -Filter "status:'new' + max_severity_displayname: 'High'" -Total
+    $DetectionsCritical = Get-FalconDetection -Filter "status:'new' + max_severity_displayname: 'Critical'" -Total
+
+    #All but "Low" =  $DetectionsCritical = Get-FalconDetection -Filter "status:'new' + max_severity_displayname: ! 'Low'" -Total
+
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>Detections new Low</channel>
+        <value>$($DetectionsLow)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>yy
+        <LimitMaxWarning>0</LimitMaxWarning>
+        </result>
+        <result>
+        <channel>Detections new Medium</channel>
+        <value>$($DetectionsMedium)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0</LimitMaxError>
+        </result>
+        <result>
+        <channel>Detections new High</channel>
+        <value>$($DetectionsHigh)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0</LimitMaxError>
+        </result>
+        <result>
+        <channel>Detections new Critical</channel>
+        <value>$($DetectionsCritical)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0</LimitMaxError>
+        </result>
+        "	
+#End Region Detections
+
+
+#Start Region Incidents
+    $Incidents = Get-FalconIncident -Filter "state: 'open'" -Total
+
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>Incidents open</channel>
+        <value>$($Incidents)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0</LimitMaxError>
+        </result>"	
+#End Region Incidents
+
+#Start Region Quarantine
+    $Quarantine = Get-FalconQuarantine -Total
+
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>Quarantine Files</channel>
+        <value>$($Quarantine)</value>
+        <unit>Count</unit>
+        <limitmode>1</limitmode>
+        <LimitMaxError>0</LimitMaxError>
+        </result>"
+#End Region Quarantine
+
+
+#Start Region Clients
+    $Hosts_Total = Get-FalconHost -Total
+    $date = ((Get-Date).AddDays(-30)).ToString("yyyy-MM-dd")
+    $Host_LastSeen = Get-FalconHost -Filter "last_seen:<=`'$($date)`'" -Total
+
+    $xmlOutput = $xmlOutput + "<result>
+        <channel>Hosts Total</channel>
+        <value>$($Hosts_Total)</value>
+        <unit>Count</unit>
+        </result>
+        <result>
+        <channel>Host lastseen > 30 Days</channel>
+        <value>$($Host_LastSeen)</value>
+        <unit>Count</unit>
+        </result>"
+
+#End Region Clients
+
+$xmlOutput = $xmlOutput + "</prtg>"
+
+$xmlOutput
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# PRTG-Crowdstrike
- PRTG Crowdstrike Monitoring
+# PRTG-CrowdStrike.PS1
+
+<!-- ABOUT THE PROJECT -->
+### About The Project
+Project Owner: Jannos-443
+
+PRTG Powershell Script to monitor CrowdStrike
+
+Free and open source: [MIT License](https://github.com/Jannos-443/PRTG-CrowdStrike/blob/main/LICENSE)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+1. Log into CrowdStrike
+   
+   - add new API/Token
+
+   - You need "devices:read, incidents:read, detects:read and quarantine:read" permission
+   
+
+2. Make sure the FalconPS Powershell Module exists on the Probe
+   - https://github.com/CrowdStrike/psfalcon/wiki
+
+
+3. Create new Sensor
+
+   | Settings | Value |
+   | --- | --- |
+   | EXE/Script Advanced | PRTG-CrowdStrike.ps1 |
+   | Parameters | -ClientId "YourCrowdStrikeAPIClientID" -ClientSecret "YourCrowdStrikeAPIClientSecret"|
+   | Scanning Interval | 30 minutes |
+
+Default API Hostname is "https://api.eu-1.crowdstrike.com" use -Hostname to change it to your Cloud
+
+## Usage
+
+```powershell
+PRTG-CrowdStrike.ps1 -ClientId "YourCrowdStrikeAPIClientID" -ClientSecret "YourCrowdStrikeAPIClientSecret" -Hostname "https://api.eu-1.crowdstrike.com"
+```
+
+![PRTG-CrowdStrike](media/ok.png)
+
+![PRTG-CrowdStrike](media/warning.png)
